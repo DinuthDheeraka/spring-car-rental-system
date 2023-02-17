6b36dc653d1da0c05d827d2941cb89258b9c1781
DinuthDheeraka <dinuthdheeraka345@gmail.com>
2023-02-18 05:13:47 +0530

    changed styles

--- SPRING-CAR-RENTAL-FRONT-END/car-rental-spa/assets/styles/js/home.js
+++ SPRING-CAR-RENTAL-FRONT-END/car-rental-spa/assets/styles/js/home.js
@@ -3,7 +3,7 @@ $('#register-cus').click(function () {
     $('#login-section').css('visibility','hidden');
     $('#all-customer-section').css('visibility','hidden');
     $('#all-driver-section').css('visibility','hidden');
-
+    $('#register-car-section').css('visibility','hidden');
 });
 
 $('#login-btn-1').click(function () {
@@ -11,7 +11,7 @@ $('#login-btn-1').click(function () {
     $('#login-section').css('visibility','visible');
     $('#all-customer-section').css('visibility','hidden');
     $('#all-driver-section').css('visibility','hidden');
-
+    $('#register-car-section').css('visibility','hidden');
 });
 
 $('#all-cus').click(function () {
@@ -19,12 +19,21 @@ $('#all-cus').click(function () {
     $('#login-section').css('visibility','hidden');
     $('#all-customer-section').css('visibility','visible');
     $('#all-driver-section').css('visibility','hidden');
-
+    $('#register-car-section').css('visibility','hidden');
 });
 
 $('#all-dri').click(function () {
     $('#customer-register-section').css('visibility','hidden');
     $('#login-section').css('visibility','hidden');
-    $('#all-customer-section').css('visibility','visible');
+    $('#all-customer-section').css('visibility','hidden');
     $('#all-driver-section').css('visibility','visible');
+    $('#register-car-section').css('visibility','hidden');
+});
+
+$('#car-reg').click(function () {
+    $('#customer-register-section').css('visibility','hidden');
+    $('#login-section').css('visibility','hidden');
+    $('#all-customer-section').css('visibility','hidden');
+    $('#all-driver-section').css('visibility','hidden');
+    $('#register-car-section').css('visibility','visible');
 });
