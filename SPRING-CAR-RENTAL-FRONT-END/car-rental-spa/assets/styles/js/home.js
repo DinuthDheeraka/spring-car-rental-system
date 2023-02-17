e680c09014a832121c053ef82c452926c41df5c1
DinuthDheeraka <dinuthdheeraka345@gmail.com>
2023-02-18 04:15:05 +0530

    added car registration form styles

--- SPRING-CAR-RENTAL-FRONT-END/car-rental-spa/assets/styles/js/home.js
+++ SPRING-CAR-RENTAL-FRONT-END/car-rental-spa/assets/styles/js/home.js
@@ -2,16 +2,29 @@ $('#register-cus').click(function () {
     $('#customer-register-section').css('visibility','visible');
     $('#login-section').css('visibility','hidden');
     $('#all-customer-section').css('visibility','hidden');
+    $('#all-driver-section').css('visibility','hidden');
+
 });
 
 $('#login-btn-1').click(function () {
     $('#customer-register-section').css('visibility','hidden');
     $('#login-section').css('visibility','visible');
     $('#all-customer-section').css('visibility','hidden');
+    $('#all-driver-section').css('visibility','hidden');
+
 });
 
 $('#all-cus').click(function () {
     $('#customer-register-section').css('visibility','hidden');
     $('#login-section').css('visibility','hidden');
     $('#all-customer-section').css('visibility','visible');
+    $('#all-driver-section').css('visibility','hidden');
+
+});
+
+$('#all-dri').click(function () {
+    $('#customer-register-section').css('visibility','hidden');
+    $('#login-section').css('visibility','hidden');
+    $('#all-customer-section').css('visibility','visible');
+    $('#all-driver-section').css('visibility','visible');
 });
