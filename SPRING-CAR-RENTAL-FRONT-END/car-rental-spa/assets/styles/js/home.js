e1809204d84bfb98905512df621231844d1e5a6a
DinuthDheeraka <dinuthdheeraka345@gmail.com>
2023-02-18 02:24:57 +0530

    changed all customer form

--- SPRING-CAR-RENTAL-FRONT-END/car-rental-spa/assets/styles/js/home.js
+++ SPRING-CAR-RENTAL-FRONT-END/car-rental-spa/assets/styles/js/home.js
@@ -1,9 +1,17 @@
 $('#register-cus').click(function () {
     $('#customer-register-section').css('visibility','visible');
     $('#login-section').css('visibility','hidden');
+    $('#all-customer-section').css('visibility','hidden');
 });
 
 $('#login-btn-1').click(function () {
     $('#customer-register-section').css('visibility','hidden');
     $('#login-section').css('visibility','visible');
+    $('#all-customer-section').css('visibility','hidden');
+});
+
+$('#all-cus').click(function () {
+    $('#customer-register-section').css('visibility','hidden');
+    $('#login-section').css('visibility','hidden');
+    $('#all-customer-section').css('visibility','visible');
 });
