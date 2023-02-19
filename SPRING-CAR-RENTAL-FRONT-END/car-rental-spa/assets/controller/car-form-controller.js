92e857419a3f50e32b50ac5635d32cd5b55b5623
DinuthDheeraka <dinuthdheeraka345@gmail.com>
2023-02-20 04:31:10 +0530

    added all cars

--- SPRING-CAR-RENTAL-FRONT-END/car-rental-spa/assets/controller/car-form-controller.js
+++ SPRING-CAR-RENTAL-FRONT-END/car-rental-spa/assets/controller/car-form-controller.js
@@ -3,11 +3,17 @@ let clickCount = 1;
 $('#add').click(function () {
     if(clickCount%2==1){
         $('#cars-container').append
-        ("<section class='row'><section class='row'><section id='car-img' class='col-5'></section><section class='col-7 car-details'><section><span class=\"badge badge-success rounded-pill d-inline\">Booked</span></section>   <section><h5>Mustang Shelby 2021</h5></section>  <section>Rs.1000.00 per day | Rs.10000.00 per month</section>  <section></section> </section></section></section>"
+        ("<section class='row'><section class='row'><section id='car-img' class='col-5'></section><section class='col-7 car-details'><section class='car-status'><span class=\"badge badge-success rounded-pill d-inline\">Available</span></section>   <section class='car-name'><h3>Mustang Shelby 2021-Auto</h3></section>  <section class='car-price'><section> <section class='price'>Rs.5000.00</section><section class='price'>Rs.50 000.00</section>  </section> <section> <section>Daily Rate</section><section>Monthly Rate</section></section> </section>  <section class='car-selection row'><section class='col-6'><div class=\"form-check\">\n" +
+            "  <input class=\"form-check-input\" type=\"checkbox\" value=\"\" id=\"flexCheckDefault\" />\n" +
+            "  <label class=\"form-check-label\" for=\"flexCheckDefault\">Select this car</label>\n" +
+            "</div></section> <section class='col-6'><button>View</button></section>  </section> </section></section></section>"
         );
     }else{
         $('#cars-container').append
-        ("<section class='row'><section class='row'><section id='car-img' class='col-5'></section><section class='col-7 car-details'><section><span class=\"badge badge-danger rounded-pill d-inline\">Booked</span></section>   <section><h5>Mustang Shelby 2021</h5></section>  <section>Rs.1000.00 per day | Rs.10000.00 per month</section>  <section></section> </section></section></section>"
+        ("<section class='row'><section class='row'><section id='car-img' class='col-5'></section><section class='col-7 car-details'><section class='car-status'><span class=\"badge badge-danger rounded-pill d-inline\">Booked</span></section>   <section class='car-name'><h3>Mustang Shelby 2021-Auto</h3></section>  <section class='car-price'><section> <section class='price'>Rs.5000.00</section><section class='price'>Rs.50 000.00</section>  </section> <section> <section>Daily Rate</section><section>Monthly Rate</section></section> </section>  <section class='car-selection row'><section class='col-6'><div class=\"form-check\">\n" +
+            "  <input class=\"form-check-input\" type=\"checkbox\" value=\"\" id=\"flexCheckDefault\" />\n" +
+            "  <label class=\"form-check-label\" for=\"flexCheckDefault\">Select this car</label>\n" +
+            "</div></section> <section class='col-6'><button id='odd-btn'>View</button></section>  </section> </section></section></section>"
         );
     }
     clickCount++;
