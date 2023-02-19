27319f765b0694d5bd5b5cfaf08d9b07fe83ab22
DinuthDheeraka <dinuthdheeraka345@gmail.com>
2023-02-20 01:45:55 +0530

    changed all cars form

--- SPRING-CAR-RENTAL-FRONT-END/car-rental-spa/assets/controller/car-form-controller.js
+++ SPRING-CAR-RENTAL-FRONT-END/car-rental-spa/assets/controller/car-form-controller.js
@@ -1,3 +1,5 @@
 $('#add').click(function () {
-    $('#cars-container').append("<section class='row' style='border: 1px solid red'></section>");
+    $('#cars-container').append
+    ("<section class='row' style='border: 1px solid red'><section class='row' style='border: 1px solid black'></section></section>"
+    );
 });
