686c7a849a6a7fa69fea6bdde6435106d4f51ddb
DinuthDheeraka <dinuthdheeraka345@gmail.com>
2023-02-20 03:46:47 +0530

    updated

--- SPRING-CAR-RENTAL-FRONT-END/car-rental-spa/assets/controller/car-form-controller.js
+++ SPRING-CAR-RENTAL-FRONT-END/car-rental-spa/assets/controller/car-form-controller.js
@@ -1,5 +1,14 @@
+
+let clickCount = 1;
 $('#add').click(function () {
-    $('#cars-container').append
-    ("<section class='row' style='border: 1px solid red'><section class='row' style='border: 1px solid black'></section></section>"
-    );
+    if(clickCount%2==1){
+        $('#cars-container').append
+        ("<section class='row'><section class='row'><section id='car-img' class='col-5'></section><section class='col-7 car-details'><section><span class=\"badge badge-success rounded-pill d-inline\">Booked</span></section>   <section><h5>Mustang Shelby 2021</h5></section>  <section>Rs.1000.00 per day | Rs.10000.00 per month</section>  <section></section> </section></section></section>"
+        );
+    }else{
+        $('#cars-container').append
+        ("<section class='row'><section class='row'><section id='car-img' class='col-5'></section><section class='col-7 car-details'><section><span class=\"badge badge-danger rounded-pill d-inline\">Booked</span></section>   <section><h5>Mustang Shelby 2021</h5></section>  <section>Rs.1000.00 per day | Rs.10000.00 per month</section>  <section></section> </section></section></section>"
+        );
+    }
+    clickCount++;
 });
