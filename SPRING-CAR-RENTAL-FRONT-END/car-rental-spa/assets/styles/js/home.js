e768d063c77a6f464e705838507b8a13466dcbbc
DinuthDheeraka <dinuthdheeraka345@gmail.com>
2023-02-21 01:00:02 +0530

    updated car card

--- SPRING-CAR-RENTAL-FRONT-END/car-rental-spa/assets/styles/js/home.js
+++ SPRING-CAR-RENTAL-FRONT-END/car-rental-spa/assets/styles/js/home.js
@@ -5,6 +5,7 @@ $('#register-cus').click(function () {
     $('#all-driver-section').css('visibility','hidden');
     $('#register-car-section').css('visibility','hidden');
     $('#driver-register-section').css('visibility','hidden');
+    $('#cars-section').css('visibility','hidden');
 });
 
 $('#login-btn-1').click(function () {
@@ -14,6 +15,7 @@ $('#login-btn-1').click(function () {
     $('#all-driver-section').css('visibility','hidden');
     $('#register-car-section').css('visibility','hidden');
     $('#driver-register-section').css('visibility','hidden');
+    $('#cars-section').css('visibility','hidden');
 });
 
 $('#all-cus').click(function () {
@@ -23,6 +25,7 @@ $('#all-cus').click(function () {
     $('#all-driver-section').css('visibility','hidden');
     $('#register-car-section').css('visibility','hidden');
     $('#driver-register-section').css('visibility','hidden');
+    $('#cars-section').css('visibility','hidden');
 });
 
 $('#all-dri').click(function () {
@@ -32,6 +35,7 @@ $('#all-dri').click(function () {
     $('#all-driver-section').css('visibility','visible');
     $('#register-car-section').css('visibility','hidden');
     $('#driver-register-section').css('visibility','hidden');
+    $('#cars-section').css('visibility','hidden');
 });
 
 $('#car-reg').click(function () {
@@ -41,6 +45,7 @@ $('#car-reg').click(function () {
     $('#all-driver-section').css('visibility','hidden');
     $('#register-car-section').css('visibility','visible');
     $('#driver-register-section').css('visibility','hidden');
+    $('#cars-section').css('visibility','hidden');
 });
 
 $('#dri-reg').click(function () {
@@ -50,4 +55,15 @@ $('#dri-reg').click(function () {
     $('#all-driver-section').css('visibility','hidden');
     $('#register-car-section').css('visibility','hidden');
     $('#driver-register-section').css('visibility','visible');
+    $('#cars-section').css('visibility','hidden');
+});
+
+$('#all-cars').click(function () {
+    $('#customer-register-section').css('visibility','hidden');
+    $('#login-section').css('visibility','hidden');
+    $('#all-customer-section').css('visibility','hidden');
+    $('#all-driver-section').css('visibility','hidden');
+    $('#register-car-section').css('visibility','hidden');
+    $('#driver-register-section').css('visibility','hidden');
+    $('#cars-section').css('visibility','visible');
 });
