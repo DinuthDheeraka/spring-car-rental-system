d2a5a51462efa7795ab30c2e46ca139c3bd6a2fa
DinuthDheeraka <dinuthdheeraka345@gmail.com>
2023-02-26 22:45:51 +0530

    updated show orders

--- SPRING-CAR-RENTAL-FRONT-END/car-rental-spa/assets/controller/show-all-orders-form-controller.js
+++ SPRING-CAR-RENTAL-FRONT-END/car-rental-spa/assets/controller/show-all-orders-form-controller.js
@@ -1,10 +1,49 @@
 let allOrders = [];
 let allCustomers = [];
 
-function setOrderTblData() {
+loadOrderTblData();
+
+function loadOrderTblData() {
     findAllOrders();
     findAllCustomers();
+    setOrderTblData();
 }
+
+function setOrderTblData() {
+    for(let i = 0; i<allOrders.length; i++){
+        let order = allOrders[i];
+        let customer = findCustomer(allOrders[i].customerId);
+        console.log(customer);
+
+        $('#admin-requests-tbl-body').append(
+            '<tr>\n' +
+            '                            <td>\n' +
+            '                                <p class="fw-bold mb-1">'+order.orderId+'</p>\n' +
+            '                            </td>\n' +
+            '                            <td>\n' +
+            '                                <p class="fw-bold mb-1">'+customer.fullName+'</p>\n' +
+            '                            </td>\n' +
+            '                            <td>\n' +
+            '                                <p class="fw-bold mb-1">'+customer.nicNumber+'</p>\n' +
+            '                            </td>\n' +
+            '                            <td>\n' +
+            '                                <p class="fw-bold mb-1">'+customer.drivingLicenseNumber+'</p>\n' +
+            '                            </td>\n' +
+            '                            <td>\n' +
+            '                                <p class="fw-bold mb-1">'+customer.emailAddress+'</p>\n' +
+            '                            </td>\n' +
+            '                            <td>\n' +
+            '                                <p class="fw-bold mb-1">'+customer.telephoneNumber+'</p>\n' +
+            '                            </td>\n' +
+            '                            <td><p class="fw-bold mb-1">'+order.orderRequestedDate+'</p></td>\n' +
+            '                            <td>\n' +
+            '                                <button class="btn btn-success btn-sm btn-rounded" type="button">View</button>\n' +
+            '                            </td>\n' +
+            '                        </tr>'
+        );
+    }
+}
+
 function findAllOrders() {
     $.ajax({
         url:baseUrl+"order/findAllOrders",
@@ -27,4 +66,12 @@ function findAllCustomers() {
             allCustomers = resp.data;
         }
     });
+}
+
+function findCustomer(id) {
+    for(let i = 0; i<allCustomers.length; i++){
+        if(allCustomers[i].customerId+''===id){
+            return allCustomers[i];
+        }
+    }
 }
